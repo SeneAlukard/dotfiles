3c5b8a6db5e6e858083a898b80842ffec75997be
senealukard <koc.ezenarslan@proton.me>
2024-07-30 22:01:24 +0300

    Optimized for stow and added neocodium and configured lsp server better

---
+++ nvim/.config/nvim/lua/senealukard/remap.lua
@@ -0,0 +1,3 @@
+vim.g.mapleader = " " 
+vim.g.maplocalleader = "\\"
+vim.api.nvim_set_keymap('n', '<leader>pv',':Ex<CR>', {noremap = true, silent = true})
---
+++ nvim/.config/nvim/lua/senealukard/settings.lua
@@ -0,0 +1,13 @@
+--Setting for NVIM--
+vim.cmd("set expandtab")
+
+vim.cmd("set tabstop=2")
+
+vim.cmd("set softtabstop=2")
+
+vim.cmd("set shiftwidth=2")
+
+vim.cmd("set number")
+
+vim.cmd("set relativenumber")
+
